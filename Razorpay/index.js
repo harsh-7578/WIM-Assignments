d89e4835706eaf8ad8e90125f13c4bfbb6656da9
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-10-17 23:53:07 +0530

    online payment api

---
+++ Razorpay/index.js
@@ -0,0 +1,26 @@
+var options = {
+    "key": "rzp_test_fHCK8X7zNkXhgD", 
+    "amount": "500",
+    "currency": "INR",
+    "name": "Acme Corp",
+    "description": "Test Transaction",
+    "image": "https://example.com/your_logo",
+    "order_id": "order_KUvQdjHw1g61ZT", 
+    "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+    "prefill": {
+        "name": "Gaurav Kumar",
+        "email": "gaurav.kumar@example.com",
+        "contact": "9999999999"
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+var rzp1 = new Razorpay(options);
+document.getElementById('rzp-button1').onclick = function(e){
+    rzp1.open();
+    e.preventDefault();
+}
